6210770af4bb372a0f496f9b5d30d74891b21c40
Sairaju27 <kraju9589@gmail.com>
2024-06-19 11:09:17 +0530

    added babel package

--- src/SideNav.js
+++ src/SideNav.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Nav, NavItem, NavLogo, NavLink } from './styles';
+// import { Nav, NavItem, NavLogo, NavLink } from './styles';
 import { Link } from 'react-router-dom';
 import "./App.css"
 const SideNav = () => {
